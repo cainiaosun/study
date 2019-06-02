1c4fbae46aa1896a8e22ba0586a680c8dac4293a
cainiaosun <1551577567@qq.com>
2019-06-02 11:56:39 +0800

    1.txt

--- 提交文件到远程.sh
+++ 提交文件到远程.sh
@@ -1,7 +1,9 @@
 echo 提交文件
 rm -f .git/index.lock
 ping -n 5 127.1>H:/可删除临时文件
-git commit -am .
+git add *
+git add .
+git commit -m *
 git push -u origin master
 echo 文件提交完成
 ping -n 30 127.1>H:/可删除临时文件
