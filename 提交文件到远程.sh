e55779e5eef51bff01268c192f1241d24dbbaba6
cainiaosun <1551577567@qq.com>
2019-06-02 14:07:46 +0800

    -a

--- 提交文件到远程.sh
+++ 提交文件到远程.sh
@@ -1,15 +1,9 @@
 echo 提交文件
-%删除锁%
-rm -f .git/index.lock
-%等待5s%
-ping -n 5 127.1>H:/可删除临时文件
-%更新本地库文件%
-git pull 
-%添加所有的文件%
-git add -A
-%提交所有的文件% 
-git commit -m -a 
-%提交文件到远程%
-git push -u origin master 
+rm -f .git/index.lock #删除锁
+ping -n 5 127.1>H:/可删除临时文件 #等待5s
+git pull #更新本地库文件
+git add -A #添加所有的文件
+git commit -m -a #提交所有的文件%
+git push -u origin master #提交文件到远程
 echo 文件提交完成
 ping -n 30 127.1>H:/可删除临时文件
